1a8fb80a2cc4ed60c03194cfcbb7e7845fc29ca0
Danny <daniel@winningwebdesign.co.uk>
2020-02-21 14:42:48 +0000

    added proper html, head, body tags etc

--- index.php
+++ index.php
@@ -1,61 +1,77 @@
 <?php require_once "core/init.php"; ?>
+<!doctype html>
+<html>
 
-<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
-<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <head>
 
-<div class="error-bar"></div>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
 
-<header style="text-align: center; width: 300px; margin: 0 auto;">
+        <title>Brain Game | Winning Web</title>
 
-    <h1><span class="blue">B</span><span class="green">r</span><span class="black">a</span><span class="yellow">i</span><span class="red">n</span> Game</h1>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
-    <p>Each round, five coloured tiles are randomly sorted into the tile grid. You will be shown the coloured tiles for 1 second, try and take them in as best you can!</p>
+    </head>
 
-    <p>Of the five available coloured tiles, you'll be asked to remember the location of one of them. Simply click the tile you think contains the required colour - try to score as many points as you can!</p>
+    <body>
 
-    <div class="high-scores" id="high-score-container">
+    <div class="error-bar"></div>
 
-        <?php include "high-scores.php"; ?>
+    <header style="text-align: center; width: 300px; margin: 0 auto;">
 
-    </div>
+        <h1><span class="blue">B</span><span class="green">r</span><span class="black">a</span><span class="yellow">i</span><span class="red">n</span> Game</h1>
 
-</header>
+        <p>Each round, five coloured tiles are randomly sorted into the tile grid. You will be shown the coloured tiles for 1 second, try and take them in as best you can!</p>
 
-<main>
+        <p>Of the five available coloured tiles, you'll be asked to remember the location of one of them. Simply click the tile you think contains the required colour - try to score as many points as you can!</p>
 
-    <div id="score-panel">Your Score: <span id="score"></span></div>
+        <div class="high-scores" id="high-score-container">
 
-    <div id="game">
+            <?php include "high-scores.php"; ?>
 
-        <?php include "game.php"; ?>
+        </div>
 
-    </div>
+    </header>
 
-    <script src="assets/js/game.js"></script>
+    <main>
 
-    <div id="ui">
+        <div id="score-panel">Your Score: <span id="score"></span></div>
 
-        <div class="instructions">
+        <div id="game">
 
-            <div id="required-color"></div>
+            <?php include "game.php"; ?>
 
-            <div>
+        </div>
 
-                <button id="start-button" onclick="newRound();"><span id="button-text">New Game!</span></button>
+        <script src="assets/js/game.js"></script>
+
+        <div id="ui">
+
+            <div class="instructions">
+
+                <div id="required-color"></div>
+
+                <div>
+
+                    <button id="start-button" onclick="newRound();"><span id="button-text">New Game!</span></button>
+
+                </div>
 
             </div>
 
         </div>
 
-    </div>
+        <div id="my-vog"></div>
 
-    <div id="my-vog"></div>
+        <script src="https://tools.winningweb.co.uk/view-on-github/ViewOnGithub.js"></script>
+        <script>
+            new ViewOnGithub( "my-vog", "Danny-XCII", "brain-game", "green" );
+        </script>
 
-    <script src="https://tools.winningweb.co.uk/view-on-github/ViewOnGithub.js"></script>
-    <script>
-        new ViewOnGithub( "my-vog", "Danny-XCII", "brain-game", "green" );
-    </script>
+        <style>#my-vog img { color: #fff; }</style>
 
-    <style>#my-vog img { color: #fff; }</style>
+    </main>
 
-</main>
+    </body>
+
+</html>
